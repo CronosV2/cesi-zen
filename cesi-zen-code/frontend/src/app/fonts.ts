8e35ce74955581eb5b3231419a92f0345848b389
Cronos <sanderusroleplay@gmail.com>
2025-08-20 15:47:09 +0200

    Add Marianne font files and update font paths in fonts.ts for correct references

--- cesi-zen-code/frontend/src/app/fonts.ts
+++ cesi-zen-code/frontend/src/app/fonts.ts
@@ -3,17 +3,17 @@ import localFont from 'next/font/local';
 export const marianne = localFont({
   src: [
     {
-      path: '../app/fonts/Marianne-Regular.woff2',
+      path: '/fonts/Marianne-Regular.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../app/fonts/Marianne-Medium.woff2',
+      path: '/fonts/Marianne-Medium.woff2',
       weight: '500',
       style: 'normal',
     },
     {
-      path: '../app/fonts/Marianne-Bold.woff2',
+      path: '/fonts/Marianne-Bold.woff2',
       weight: '700',
       style: 'normal',
     },
